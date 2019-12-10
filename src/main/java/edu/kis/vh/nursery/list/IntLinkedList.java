dc27791479038b17b938eaad069e42fa4992e439
Graleszczur <Grales76@onet.pl>
2019-12-10 14:31:22 +0100

    add java doc skeleton

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,5 +1,6 @@
 package edu.kis.vh.nursery.list;
 
+
 public class IntLinkedList {
 
 	Node last;
--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,10 +1,20 @@
 package edu.kis.vh.nursery.list;
-
+/**
+ * Class Node kept in IntLinkedList.
+ */
 public class Node {
-
+	/**
+	 * Value stored in single node
+	 */
 	public int value;
+	/**
+	 * prev holds reference to previous list element
+	 * next holds reference to next list element
+	 */
 	public Node prev, next;
-
+	/**
+	 * @param i value to assign and hold in node
+	 */
 	public Node(int i) {
 		value = i;
 	}
