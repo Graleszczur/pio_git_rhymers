c2e71df78d73ae8eb4b49699d9e302d08d119747
wawrzo <wawrzonowskipiotr@gmail.com>
2022-01-19 18:42:56 +0100

    1.4 stale 2

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,10 +2,10 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int NUMBERS_SIZE = 12;
+    private static final int NUMBERS_SIZE = 12;
     public static final int TOTAL_INITIAL_VALUE = -1;
 
-    private int[] numbers = new int[NUMBERS_SIZE];
+    private final int[] numbers = new int[NUMBERS_SIZE];
 
     public int total = TOTAL_INITIAL_VALUE;
 
